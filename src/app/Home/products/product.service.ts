d2dc5f71f773b039efc8c0d02fac6a5aae2a347b
Affan Showkat Hakak <affanshowkat@gmail.com>
2022-02-08 19:37:26 +0530

    Added product details components

--- src/app/Home/products/product.service.ts
+++ src/app/Home/products/product.service.ts
@@ -1,10 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
 import { IProduct } from './products';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  // private productUrl = '';
+  // constructor(private http: HttpClient) {}
   getProducts(): IProduct[] {
     return [
       {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Home/products/product-list.component';
 import { ConvertToSpacesPipe } from './Shared/convert-to-spaces.pipe';
 import { StarComponent } from './Shared/star.component';
 import { ProductService } from './Home/products/product.service';
+import { ProductDetailsComponent } from './Home/products/product-details.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,9 @@ import { ProductService } from './Home/products/product.service';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
