78f7adbd61918813bd848f59dce181f5fd80fe12
Affan Showkat Hakak <affanshowkat@gmail.com>
2022-02-08 22:16:34 +0530

    Added routes to the application

--- src/app/Home/products/product-list.component.ts
+++ src/app/Home/products/product-list.component.ts
@@ -3,7 +3,6 @@ import { ProductService } from './product.service';
 import { IProduct } from './products';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Home/products/product-list.component';
-import { ConvertToSpacesPipe } from './Shared/convert-to-spaces.pipe';
 import { StarComponent } from './Shared/star.component';
-import { ProductService } from './Home/products/product.service';
 import { ProductDetailsComponent } from './Home/products/product-details.component';
+import { WelcomeComponent } from './Home/products/welcome-component';
+
+import { ProductService } from './Home/products/product.service';
+import { ConvertToSpacesPipe } from './Shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
@@ -18,8 +20,19 @@ import { ProductDetailsComponent } from './Home/products/product-details.compone
     ConvertToSpacesPipe,
     StarComponent,
     ProductDetailsComponent,
+    WelcomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailsComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    ]),
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
