188ed765beaa92a67240c2bd26aabb800fe85066
Affan Showkat Hakak <affanshowkat@gmail.com>
2022-02-09 11:46:50 +0530

    added some cards

--- src/app/Home/products/product-details.component.ts
+++ src/app/Home/products/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from './products';
 import { Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-product-detail',
@@ -11,22 +12,18 @@ import { Router } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   pageTitle: string = 'Product-Detail';
   product: IProduct | undefined;
+  products: IProduct[] = [];
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
+    this.products = this.productService.getProducts();
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
-    this.product = {
-      productId: id,
-      productName: 'Electronics Store',
-      productCode: 'ABC-001',
-      releaseDate: 'March 2020',
-      description: 'Mobile Phones, Laptops and all Electronic Accessories',
-      priceStartsAt: 249.99,
-      starRating: 4,
-      imageUrl: 'assets/images/electronics.jpg',
-    };
   }
   onBack(): void {
     this.router.navigate(['/products']);
